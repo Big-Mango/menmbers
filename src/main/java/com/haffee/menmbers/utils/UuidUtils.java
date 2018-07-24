7e617a57af2e1369e8007b410be4201676d266ac
tongjichang <459132449@qq.com>
2018-07-24 20:48:39 +0800

    工具类

---
+++ src/main/java/com/haffee/menmbers/utils/UuidUtils.java
@@ -0,0 +1,16 @@
+package com.haffee.menmbers.utils;
+
+import java.util.UUID;
+
+/**
+ * create by jacktong
+ * date 2018/7/24 下午8:25
+ **/
+
+public class UuidUtils {
+
+    public static String getUUID32(){
+        String uuid = UUID.randomUUID().toString().replace("-", "").toLowerCase();
+        return uuid;
+    }
+}
