0027e5eb12ba98b172b63b8b940c45bd4213ad94
tongjichang <459132449@qq.com>
2018-07-27 21:18:32 +0800

    登录、注销功能开发

---
+++ src/main/java/com/haffee/menmbers/service/BaseService.java
@@ -0,0 +1,16 @@
+package com.haffee.menmbers.service;
+
+import com.haffee.menmbers.entity.SysCode;
+
+import java.util.List;
+
+/**
+ * create by jacktong
+ * date 2018/7/26 下午6:29
+ * 通用接口--基础功能
+ **/
+
+public interface BaseService {
+
+    List<SysCode> findByCode(String code) throws Exception;
+}
---
+++ src/main/java/com/haffee/menmbers/service/impl/BaseServiceImpl.java
@@ -0,0 +1,34 @@
+package com.haffee.menmbers.service.impl;
+
+import com.haffee.menmbers.entity.SysCode;
+import com.haffee.menmbers.repository.SysCodeRepository;
+import com.haffee.menmbers.service.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * create by jacktong
+ * date 2018/7/26 下午6:30
+ **/
+
+@Service
+@Transactional
+public class BaseServiceImpl implements BaseService {
+
+    @Autowired
+    private SysCodeRepository sysCodeRepository;
+
+    /**
+     * 根据code查询二级代码
+     * @param code
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public List<SysCode> findByCode(String code) throws Exception {
+        return sysCodeRepository.findbyCode(code);
+    }
+}
